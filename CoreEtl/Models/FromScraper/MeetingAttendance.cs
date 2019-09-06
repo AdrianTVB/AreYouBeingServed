a25326f9bd66fcbfe48c8d548ef6db598ca335b0
gabriel <gabriel.ward@re-leased.com>
2019-09-06 22:48:19 +1200

    unfinished CSV to c# import

---
+++ CoreEtl/Models/FromScraper/MeetingAttendance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreEtl.Models.FromScraper
+{
+	class MeetingAttendance
+	{
+		public string Organisation { get; set; }
+		public string Date { get; set; }
+		public string Meeting { get; set; }
+	}
+}

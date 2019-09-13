c152e7b485b6a7a0483ab2c65554971161504dd0
jeffcnz <jeffjcooke@hotmail.com>
2019-09-13 17:43:23 +1200

    Initial data model

---
+++ scrape/scripts/baseinfo.py
@@ -0,0 +1,30 @@
+# Base information for loading into database
+
+organisations = {
+    [
+        {
+            "orgName": "Napier City Council",
+            "shortName": "NCC"
+        },
+        {
+            "orgName": "Hastings District Council",
+            "shortName": "HDC"
+        },
+        {
+            "orgName": "Central Hawke's Bay District Council",
+            "shortName": "CHBDC"
+        },
+        {
+            "orgName": "Hawke's Bay Regional Council",
+            "shortName": "HBRC"
+        },
+        {
+            "orgName": "Wairoa District Council",
+            "shortName": "WDC"
+        },
+        {
+            "orgName": "Hawke's Bay District Health Board",
+            "shortName": "HBDHB"
+        }
+    ]
+}
---
+++ scrape/scripts/dbModel.py
@@ -0,0 +1,98 @@
+# Import Functions
+
+from sqlalchemy import create_engine, ForeignKey, Column, Integer, String, Date, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+engine = create_engine('sqlite:///rubs.db')
+
+Base = declarative_base()
+
+
+class Organisation(Base):
+     __tablename__ = 'organisations'
+
+     orgID = Column(Integer, primary_key=True)
+     orgName = Column(String(100))
+     shortName = Column(String(50))
+
+     def __repr__(self):
+        return "<Organisation(orgName='%s', shortName='%s')>" % (
+                             self.orgName, self.shortName)
+
+
+class Representative(Base):
+    __tablename__ = 'representatives'
+
+    repID = Column(Integer, primary_key=True)
+    surname = Column(String(50))
+    forename = Column(String(50))
+
+    def __repr__(self):
+       return "<Representative(Forename='%s', Surname='%s')>" % (
+                            self.forename, self.surname)
+
+
+class Role(Base):
+    __tablename__ = 'roles'
+
+    roleID = Column(Integer, primary_key=True)
+    roleName = Column(String(50))
+
+    def __repr__(self):
+       return "<Role(Name='%s')>" % (
+                            self.roleName)
+
+
+class MeetingType(Base):
+    __tablename__ = 'meetingTypes'
+
+    meetTypeID = Column(Integer, primary_key=True)
+    meetName = Column(String(100))
+
+    def __repr__(self):
+       return "<MeetingType(Name='%s')>" % (
+                            self.meetName)
+
+
+class MeetingRepRelationship(Base):
+    __tablename__ = 'meetingRepRelationships'
+
+    relID = Column(Integer, primary_key=True)
+    orgID = Column(Integer, ForeignKey('organisations.orgID'))
+    repID = Column(Integer, ForeignKey('representatives.repID'))
+    meetTypeID = Column(Integer, ForeignKey('meetingTypes.meetTypeID'))
+    startDate = Column(Date())
+    endDate = Column(Date())
+
+    organisation = relationship("Organisation", back_populates="meetingRepRelationships")
+    representatives = relationship("Representative", back_populates='meetingRepRelationships')
+    meetingType = relationship("MeetingType", back_populates='meetingTypes')
+
+
+class Meetings(Base):
+    __tablename__ = 'meetings'
+
+    meetID = Column(Integer, primary_key=True)
+    orgID = Column(Integer, ForeignKey('organisations.orgID'))
+    meetTypeID = Column(Integer, ForeignKey('meetingTypes.meetTypeID'))
+    meetDate = Column(Date())
+    minuteUrl = Column(String(200))
+    minuteType = Column(String(4)) # html or pdf
+    minuteText = Column(Text())
+    minuteFile = Column(String(50))
+
+    organisation = relationship("Organisation", back_populates="meetings")
+    meetingType = relationship("MeetingType", back_populates='meetings')
+
+
+class MeetingAttendance(Base):
+    __tablename__ = "meetingAttendance"
+
+    meetAttID = Column(Integer, primary_key=True)
+    meetID = Column(Integer, ForeignKey('meetings.meetID'))
+    repID = Column(Integer, ForeignKey('representatives.repID'))
+
+#Organisation.representatives = relationship("Representative", order_by=Representative.id, back_populates="organisation")
+Base.metadata.create_all(engine)

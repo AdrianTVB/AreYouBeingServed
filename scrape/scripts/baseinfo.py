de680b283c0786ea8e73adb1f7bdd98f2f42b3f0
jeffcnz <jeffjcooke@hotmail.com>
2019-09-13 18:32:49 +1200

    Start of db population script

--- scrape/scripts/baseinfo.py
+++ scrape/scripts/baseinfo.py
@@ -1,30 +1,28 @@
 # Base information for loading into database
 
-organisations = {
-    [
-        {
-            "orgName": "Napier City Council",
-            "shortName": "NCC"
-        },
-        {
-            "orgName": "Hastings District Council",
-            "shortName": "HDC"
-        },
-        {
-            "orgName": "Central Hawke's Bay District Council",
-            "shortName": "CHBDC"
-        },
-        {
-            "orgName": "Hawke's Bay Regional Council",
-            "shortName": "HBRC"
-        },
-        {
-            "orgName": "Wairoa District Council",
-            "shortName": "WDC"
-        },
-        {
-            "orgName": "Hawke's Bay District Health Board",
-            "shortName": "HBDHB"
-        }
-    ]
-}
+organisations = [
+    {
+        "orgName": "Napier City Council",
+        "shortName": "NCC"
+    },
+    {
+        "orgName": "Hastings District Council",
+        "shortName": "HDC"
+    },
+    {
+        "orgName": "Central Hawke's Bay District Council",
+        "shortName": "CHBDC"
+    },
+    {
+        "orgName": "Hawke's Bay Regional Council",
+        "shortName": "HBRC"
+    },
+    {
+        "orgName": "Wairoa District Council",
+        "shortName": "WDC"
+    },
+    {
+        "orgName": "Hawke's Bay District Health Board",
+        "shortName": "HBDHB"
+    }
+]
---
+++ scrape/scripts/scraper.py
@@ -0,0 +1,37 @@
+#from sqlalchemy import create_engine, ForeignKey, Column, Integer, String, Date, Text
+#from sqlalchemy.ext.declarative import declarative_base
+#from sqlalchemy.orm import relationship
+
+import sqlalchemy as db
+import baseinfo
+
+engine = db.create_engine('sqlite:///rubs.db')
+
+connection = engine.connect()
+metadata = db.MetaData()
+organisations = db.Table('organisations', metadata, autoload=True, autoload_with=engine)
+
+newOrg = baseinfo.organisations[0]
+
+
+def orgUpdate(newOrg):
+    #Check newOrg has an orgName field
+    if not newOrg['orgName']:
+        print("Dictionary must contain an 'orgName'")
+        return -1
+    # Check if organisation already in the Table
+    exist = db.select([organisations]).where(organisations.columns.orgName == newOrg['orgName'])
+    ResultProxy = connection.execute(exist)
+    ResultSet = ResultProxy.fetchall()
+
+    print(len(ResultSet))
+    print(ResultSet)
+
+    # If it isn't then insert it
+    if not ResultSet:
+        ins = db.insert(organisations).values(orgName=newOrg['orgName'], shortName=newOrg['shortName'])
+        ResultProxy = connection.execute(ins)
+
+    # If it is then either skip or update
+
+orgUpdate(newOrg=newOrg)

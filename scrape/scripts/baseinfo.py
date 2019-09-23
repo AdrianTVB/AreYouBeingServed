e033e4a78e3f185f25803897abae1dca45b9102b
jeffcnz <jeffjcooke@hotmail.com>
2019-09-23 18:50:04 +1200

    Completed HB representatives

--- scrape/scripts/baseinfo.py
+++ scrape/scripts/baseinfo.py
@@ -109,6 +109,78 @@ representatives = [
                 "ImageUrl": "https://www.hastingsdc.govt.nz/assets/Contacts/KevinWatkins-HDCweb.jpg"
             }
         ]
+    },
+    {
+        "orgShortName": "NCC",
+        "fromDate": "",
+        "toDate": "",
+        "reps": [
+            {
+                "Surname": "White",
+                "Forename": "Faye",
+                "ImageUrl": "https://www.napier.govt.nz/assets/Contacts/Councillor-Faye-White-Oct-2016.jpg"
+            },
+            {
+                "Surname": "Dalton",
+                "Forename": "Bill",
+                "ImageUrl": "https://www.napier.govt.nz/assets/Contacts/Mayor-Bill-Dalton-Oct-2016-3.jpg"
+            },
+            {
+                "Surname": "Price",
+                "Forename": "Keith",
+                "ImageUrl": "https://www.napier.govt.nz/assets/Contacts/Councillor-Keith-Price-Oct-2016-2.jpg"
+            },
+            {
+                "Surname": "McGrath",
+                "Forename": "Richard",
+                "ImageUrl": "https://www.napier.govt.nz/assets/Contacts/Councillor-Richard-McGrath-Oct-2016-2.jpg"
+            },
+            {
+                "Surname": "Jeffery",
+                "Forename": "Tony",
+                "ImageUrl": "https://www.napier.govt.nz/assets/Contacts/Councillor-Tony-Jefferies-Mar-2017-2.jpg"
+            },
+            {
+                "Surname": "Boag",
+                "Forename": "Maxine",
+                "ImageUrl": "https://www.napier.govt.nz/assets/Contacts/Councillor-Maxine-Boag-Oct-2016-5.jpg"
+            },
+            {
+                "Surname": "Brosnan",
+                "Forename": "Annette",
+                "ImageUrl": "https://www.napier.govt.nz/assets/Contacts/Councillor-Annette-Brosnan-Oct-2016-7.jpg"
+            },
+            {
+                "Surname": "Taylor",
+                "Forename": "Graeme",
+                "ImageUrl": "https://www.napier.govt.nz/assets/Contacts/_resampled/FillWyI4MDAiLCI4MDAiXQ-Councillor-Graeme-Taylor-Oct-2016-7.jpg"
+            },
+            {
+                "Surname": "Wise",
+                "Forename": "Kirsten",
+                "ImageUrl": "https://www.napier.govt.nz/assets/Contacts/Councillor-Kirsten-Wise-Mar-2017.jpg"
+            },
+            {
+                "Surname": "Hague",
+                "Forename": "Claire",
+                "ImageUrl": "https://www.napier.govt.nz/assets/Contacts/Councillor-Claire-Hague-Oct-2016-6.jpg"
+            },
+            {
+                "Surname": "Dallimore",
+                "Forename": "Larry",
+                "ImageUrl": "https://www.napier.govt.nz/assets/Contacts/Councillor-Larry-Dallimore-Oct-2016-8.jpg"
+            },
+            {
+                "Surname": "Wright",
+                "Forename": "Tania",
+                "ImageUrl": "https://www.napier.govt.nz/assets/Contacts/Councillor-Tania-Wright-Oct-2016-12.jpg"
+            },
+            {
+                "Surname": "Tapine",
+                "Forename": "Api",
+                "ImageUrl": "https://www.napier.govt.nz/assets/Contacts/_resampled/FillWyI4MDAiLCI4MDAiXQ-Councillor-Apiata-Tapine-Oct-2016.jpg"
+            }
+        ]
     }
 ]
 
